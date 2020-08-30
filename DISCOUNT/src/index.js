beb75f947fc73081a79413c51430d9312b4075d8
tejas mokal <tejasmokal82gmail,com>
2020-08-30 23:28:16 +0530

    FSJ0820B105

--- DISCOUNT/src/index.js
+++ DISCOUNT/src/index.js
@@ -5,11 +5,14 @@
 
 
 //Write a function to return the Discount Amount
-function find_DiscountAmount(item1,item2,disc_precent){
-return null
+function find_DiscountAmount(item1, item2, disc_precent) {
+    var total = (item1 + item2)
+    var item1disc = (item1 / 100) * disc_precent
+    var item2disc = (item2 / 100) * disc_precent
+    return total - (item1disc + item2disc)
 }
 
 //Write a function to return the discount percent
-function find_Discount(item1,item2,disc_precent){
-return null
+function find_Discount(item1, item2, disc_precent) {
+    return ((disc_precent / 100) * (item1 + item2));
 }
