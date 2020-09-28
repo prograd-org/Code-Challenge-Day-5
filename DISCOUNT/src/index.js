b46033efe40ef65420d4f34fa90f5f8e42e73832
Nikhil Akash <samineninikhilakash@gmail.com>
2020-09-28 09:35:38 +0530

    code-week2-day1

--- DISCOUNT/src/index.js
+++ DISCOUNT/src/index.js
@@ -6,10 +6,11 @@
 
 //Write a function to return the Discount Amount
 function find_DiscountAmount(item1,item2,disc_precent){
-return null
+    var discount= (item1+item2)*(disc_precent/100);
+return ((item1+item2)-discount);
 }
 
 //Write a function to return the discount percent
 function find_Discount(item1,item2,disc_precent){
-return null
+return ((item1+item2)*(disc_precent/100));
 }
